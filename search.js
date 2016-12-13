8213441ba1309a3f24b5912603cd34bceb318436
Dillon Powers <dillon.c.powers@gmail.com>
2016-12-13 16:58:27 -0500

    demo time

--- search.js
+++ search.js
@@ -3,14 +3,26 @@
     chrome.tabs.executeScript({
       file: '/script.js'
     }, result => {
+      var bars = document.getElementsByClassName('gsc-input');
+      console.log('bars are', bars)
+      // want bars[1]
       document.getElementById("gsc-i-id1").value = result[0];
+
+      // simulate clicking Google search
       var btn = document.getElementsByClassName('gsc-search-button');
       btn[1].click();
     })
   }
 
-  (function() {
-    var cx = '007968424207281162508:l7pac8clyg0';
+
+
+
+
+
+
+// // MODERATE
+(function() {
+    var cx = '007780150377665373782:-n2prkuxpze';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
@@ -21,5 +33,52 @@
     var btn = document.getElementById('populateSearch');
     btn.addEventListener('click', populateSearch);
   })();
+
+
+
+// // LIBERAL
+// (function() {
+//     var cx = '007780150377665373782:3zdep6z7yg8';
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+
+//     var btn = document.getElementById('populateSearch');
+//     btn.addEventListener('click', populateSearch);
+//   })();
+
+// // CONSERVATIVE
+// (function() {
+//     var cx = '007780150377665373782:1iwvtunrrw0';
+//     var gcse = document.createElement('script');
+//     gcse.type = 'text/javascript';
+//     gcse.async = true;
+//     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(gcse, s);
+
+//     var btn = document.getElementById('populateSearch');
+//     btn.addEventListener('click', populateSearch);
+//   })();
+
+
+
+
+// OLD ONE
+  // (function() {
+  //   var cx = '007968424207281162508:l7pac8clyg0';
+  //   var gcse = document.createElement('script');
+  //   gcse.type = 'text/javascript';
+  //   gcse.async = true;
+  //   gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(gcse, s);
+
+    // var btn = document.getElementById('populateSearch');
+    // btn.addEventListener('click', populateSearch);
+  // })();
 
 
