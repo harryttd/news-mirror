9b0613d63d14014f3c9130c832422401a3da7c9e
Aryeh Harris <harryttd@gmail.com>
2016-12-13 14:18:27 -0500

    Finally got custom search in extension

--- search.js
+++ search.js
@@ -1,6 +1,16 @@
+  //
+  // (function() {
+  //   console.log("HELLO");
+  //   var cx = '007968424207281162508:l7pac8clyg0';
+  //   var gcse = document.createElement('script');
+  //   gcse.type = 'text/javascript';
+  //   gcse.async = true;
+  //   gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(gcse, s);
+  // })();
 
   (function() {
-    console.log("HELLO");
     var cx = '007968424207281162508:l7pac8clyg0';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
